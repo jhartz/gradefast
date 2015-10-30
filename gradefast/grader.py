ed6d7bf0616935a334d80273da406441de1c9e60
Jake Hartz <jhartz@outlook.com>
2015-10-30 19:24:26 -0400

    Added exception catching for bad directories/filenames

--- gradefast/grader.py
+++ gradefast/grader.py
@@ -1092,6 +1092,8 @@ class CommandRunner:
             # The output will only be collected here if we have stdout set above
             # (i.e. if we're planning on capturing the output)
             output, _ = process.communicate(input=input)
+        except (NotADirectoryError, FileNotFoundError) as ex:
+            self._io.error("Directory or file not found: " + ex)
         except (InterruptedError, KeyboardInterrupt):
             self._io.error("Process interrupted")
 
