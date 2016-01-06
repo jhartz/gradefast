b11b78dae8556dc86ddcadc28ed5bfd4e56c7e57
Jake Hartz <jhartz@outlook.com>
2016-01-06 13:47:57 -0500

    Added little script to run GradeFast module in IDEs

---
+++ run_gradefast.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Runs the GradeFast module, for use in environments where we can't just do:
+    python -m gradefast [args]
+Instead, we can do:
+    python run_gradefast.py [args]
+
+Licensed under the MIT License. For more, see the LICENSE file.
+
+Author: Jake Hartz <jhartz@mail.rit.edu>
+"""
+
+import sys
+import os
+import runpy
+
+path = os.path.dirname(sys.modules[__name__].__file__)
+path = os.path.join(path, "..")
+sys.path.insert(0, path)
+runpy.run_module("gradefast", run_name="__main__", alter_sys=True)
