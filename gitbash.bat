34197a5ace40d5406d383b41a674e84ea3b427da
Jake Hartz <jake+git@hartz.io>
2017-04-10 22:09:28 -0400

    Added gitbash.bat for Windows users
    Usage: python -m gradefast ... --shell gitbash.bat
    
    which will use the version of bash bundled with Git to interpret the commands.

---
+++ gitbash.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REM This file exists so you can use it as the "shell" option for GradeFast
+REM on Windows to process all commands through Git Bash.
+
+"C:\Program Files\Git\bin\bash.exe" -c %1
