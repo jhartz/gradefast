1bc98811cb977ab9ae5201cf65a7bff7609fbfcb
Jake Hartz <jake+git@hartz.io>
2017-05-06 16:49:03 -0400

    Add stats.py

---
+++ gradefast/stats.py
@@ -0,0 +1,7 @@
+"""
+GradeFast Stats - Keeps track of statistics on how good you are at grading.
+
+Licensed under the MIT License. For more, see the LICENSE file.
+
+Author: Jake Hartz <jake@hartz.io>
+"""
